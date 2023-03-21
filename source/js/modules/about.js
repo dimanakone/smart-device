97f20922e785f34d4f5e38a2bec2d01173bb2bed
dimanakone <dimka39@gmail.com>
2023-03-22 00:13:51 +0300

    fix

--- source/js/modules/about.js
+++ source/js/modules/about.js
@@ -1,22 +1,26 @@
-const paragraphs = document.querySelector('.about').querySelectorAll('p');
-const aboutButton = document.querySelector('.about__button');
+const about = document.querySelector('.about');
 
-paragraphs.forEach((element, index) => {
-  if (index > 1) {
-    element.classList.add('about__text-hidden');
-  }
-});
+if (about) {
+  const paragraphs = about.querySelectorAll('p');
+  const aboutButton = document.querySelector('.about__button');
 
-aboutButton.addEventListener('click', () => {
   paragraphs.forEach((element, index) => {
     if (index > 1) {
-      element.classList.toggle('about__text-hidden--active');
+      element.classList.add('about__text-hidden');
     }
   });
 
-  if (aboutButton.innerText.toLowerCase() === 'подробнее') {
-    aboutButton.innerText = 'Свернуть';
-  } else {
-    aboutButton.innerText = 'Подробнее';
-  }
-});
+  aboutButton.addEventListener('click', () => {
+    paragraphs.forEach((element, index) => {
+      if (index > 1) {
+        element.classList.toggle('about__text-hidden--active');
+      }
+    });
+
+    if (aboutButton.innerText.toLowerCase() === 'подробнее') {
+      aboutButton.innerText = 'Свернуть';
+    } else {
+      aboutButton.innerText = 'Подробнее';
+    }
+  });
+}
